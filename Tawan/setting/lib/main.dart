036c16ab5933c83acc7b0c238cadee6da0c43f98
smileNhappy <1004tawan@gmail.com>
2024-02-06 02:42:43 +0700

    setting day 1

--- Tawan/setting/lib/main.dart
+++ Tawan/setting/lib/main.dart
@@ -5,121 +5,311 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/setting',
+      routes: {
+        '/setting': (context) => const Setting(),
+        '/language': (context) => const LanguagePage(),
+        '/notification': (context) => const NotificationPage(),
+        '/theme': (context) => const ThemePage(),
+        '/helpFeedback': (context) => const HelpFeedbackPage(),
+        '/aboutUs': (context) => const AboutUsPage(),
+        '/sound': (context) => const SoundPage(),
+        '/warningDistant': (context) => const WarningDistantPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LanguagePage extends StatelessWidget {
+  const LanguagePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Text('Settings'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/setting');
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(
+        children: [
+          w('Language / ภาษา', context, 'language'),
+          w('Notification', context, 'notification'),
+          w('Theme', context, 'theme'),
+          w('Help & Feedback', context, 'helpFeedback'),
+          w('About Us', context, 'aboutUs')
+        ],
+      ),
     );
   }
+}
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notification'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/setting');
+          },
+        ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.blue,
+                  width: 2.0,
+                ),
+              ),
+            ),
+          ),
+          w('Warning distant', context, 'warningDistant'),
+          w('Sound', context, 'sound'),
+          w('Vibration', context, 'vibration'),
+        ],
+      ),
+    );
+  }
+}
+
+class SoundPage extends StatelessWidget {
+  const SoundPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notification'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/notification');
+          },
+        ),
+      ),
+      body: ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("Sound"),
+          ),
+          w('Warn with sound', context, 'warnWithSound'),
+          w('Media', context, 'media'),
+        ],
+      ),
+    );
+  }
+}
+
+class WarningDistantPage extends StatelessWidget {
+  const WarningDistantPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Warning distant'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/notification');
+          },
+        ),
+      ),
+      body: ListView(
+        children: const [
+          Text("Measurement"),
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("Distant"),
+          ),
+          Text("Be chill"),
+          Text("We're close now"),
+          Text("Prepare to leave"),
+          Text("Now!"),
+        ],
+      ),
+    );
+  }
+}
+
+class ThemePage extends StatelessWidget {
+  const ThemePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/setting');
+          },
+        ),
+      ),
+      body: ListView(
+        children: [
+          w('Language / ภาษา', context, 'language'),
+          w('Notification', context, 'notification'),
+          w('Theme', context, 'theme'),
+          w('Help & Feedback', context, 'helpFeedback'),
+          w('About Us', context, 'aboutUs')
+        ],
+      ),
+    );
+  }
+}
+
+class HelpFeedbackPage extends StatelessWidget {
+  const HelpFeedbackPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('How can we help?'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/setting');
+          },
+        ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.blue,
+                  width: 2.0,
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("HELP"),
+          ),
+          w('How to use', context, 'howToUse'),
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("Feedback"),
+          ),
+          w('GPS & Tracking', context, 'GPSTracking'),
+          w('Set notification', context, 'setNotification'),
+          w('Service', context, 'service'),
+          w('Others', context, 'others'),
+        ],
+      ),
+    );
+  }
+}
+
+class AboutUsPage extends StatelessWidget {
+  const AboutUsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/setting');
+          },
+        ),
+      ),
+      body: ListView(
+        children: [
+          w('Language / ภาษา', context, 'language'),
+          w('Notification', context, 'notification'),
+          w('Theme', context, 'theme'),
+          w('Help & Feedback', context, 'helpFeedback'),
+          w('About Us', context, 'aboutUs')
+        ],
+      ),
+    );
+  }
+}
+
+class Setting extends StatelessWidget {
+  const Setting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Center(
+            child: Text(
+          'Setting',
+          style: TextStyle(fontSize: 40),
+        )),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.blue,
+                  width: 2.0,
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("GENERAL "),
+          ),
+          w('Language / ภาษา', context, 'language'),
+          w('Notification', context, 'notification'),
+          w('Theme', context, 'theme'),
+          w('Help & Feedback', context, 'helpFeedback'),
+          w('About Us', context, 'aboutUs')
+        ],
+      ),
+    );
+  }
+}
+
+Widget w(String title, BuildContext context, String link) {
+  return ListTile(
+    tileColor: const Color.fromARGB(255, 228, 238, 246),
+    title: Row(children: [
+      Text(title),
+      const Spacer(),
+      const Text(
+        ">",
+        style: TextStyle(color: Color.fromARGB(255, 0, 153, 255)),
+      )
+    ]),
+    onTap: () {
+      Navigator.pop(context);
+      Navigator.pushNamed(context, "/$link");
+    },
+  );
 }
